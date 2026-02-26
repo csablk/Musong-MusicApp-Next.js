ff1b888d45fd4ecdbad2e2c5624edb3f7b37a835
csablk <samircsa@mail.ru>
2026-02-27 00:54:41 +0300

    first commit

---
+++ app/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Figtree } from 'next/font/google';
+import Sidebar from "@/components/Siderbar";
+import "./globals.css";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+
+const font = Figtree({ subsets: ['latin'] });
+
+
+export const metadata: Metadata = {
+  title: "Musong",
+  description: "Listen music with Musong",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider/>
+        <Sidebar>
+          {children}
+        </Sidebar>
+        </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,69 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+import useAuthModal from "@/app/hooks/useAuthModal";
+
+interface HeaderProps {
+    children: React.ReactNode; 
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({children, className}) => {
+    const authModal = useAuthModal();
+    const router = useRouter();
+
+    const handleLogout = () => {
+        
+    }
+    return ( 
+        <div className={twMerge(`
+            h-fit 
+            bg-gradient-to-b
+            from-purple-700
+            p-6
+        `)}>
+            <div className="w-full mb-4 flex items-center justify-between">
+                <div className="hidden md:flex gap-x-2 items-center">
+                    <button onClick={() => router.back()} className="rounded-full cursor-pointer items-center flex justify-center hover:opacity-75 transition">
+                        <RxCaretLeft className="text-white" size={35}/>
+                    </button>
+                     <button onClick={() => router.forward()} className="rounded-full cursor-pointer items-center flex justify-center hover:opacity-75 transition">
+                        <RxCaretRight className="text-white" size={35}/>
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <HiHome size={20} className="text-black"/>
+                    </button>
+                      <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <BiSearch size={20} className="text-black"/>
+                    </button>
+                </div>
+                <div className="flex justify-between items-center gap-x-4">
+                    <>
+                        <div>
+                            <Button onClick={authModal.onOpen} className="cursor-pointer bg-transparent text-neutral-300 font-medium">
+                                Sign up
+                            </Button>
+                        </div>
+
+                        <div>
+                            
+                            <Button onClick={authModal.onOpen} className="cursor-pointer bg-white px-6 py-2">
+                                Log in
+                            </Button>
+                        </div>
+                    </>
+                </div>
+            </div>
+            {children}
+        </div>
+     );
+}
+ 
+export default Header;
